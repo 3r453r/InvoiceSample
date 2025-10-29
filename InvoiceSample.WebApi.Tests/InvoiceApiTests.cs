81cb1c749a727cdf0ad35ac0d069694965eeba8c
3r453r <44854864+3r453r@users.noreply.github.com>
2025-10-29 17:43:59 +0100

    Fix integration test request handling

--- InvoiceSample.WebApi.Tests/InvoiceApiTests.cs
+++ InvoiceSample.WebApi.Tests/InvoiceApiTests.cs
@@ -1,12 +1,19 @@
-using RestSharp;
 using FluentAssertions;
 using InvoiceSample.WebApi.Dtos;
+using RestSharp;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Xunit;
 
 namespace InvoiceSample.WebApi.Tests;
 
 public class InvoiceApiTests : IClassFixture<TestWebApplicationFactory>
 {
+    private static readonly JsonSerializerOptions SerializerOptions = new(JsonSerializerDefaults.Web)
+    {
+        ReferenceHandler = ReferenceHandler.Preserve
+    };
+
     private readonly RestClient _client;
 
     public InvoiceApiTests(TestWebApplicationFactory factory)
@@ -39,14 +46,17 @@ public class InvoiceApiTests : IClassFixture<TestWebApplicationFactory>
         };
 
         var postRequest = new RestRequest("/SalesOrder").AddJsonBody(request);
-        var postResponse = await _client.ExecutePostAsync<InvoiceResponse>(postRequest);
+        var postResponse = await _client.ExecutePostAsync(postRequest);
         postResponse.IsSuccessful.Should().BeTrue();
-        var invoiceNumber = postResponse.Data!.Number;
+        var createdInvoice = Deserialize(postResponse);
 
-        var getRequest = new RestRequest($"/Invoice/{invoiceNumber}");
-        var getResponse = await _client.ExecuteGetAsync<InvoiceResponse>(getRequest);
+        var invoiceNumber = createdInvoice.Number!;
+
+        var getRequest = new RestRequest("/Invoice/{number}").AddUrlSegment("number", invoiceNumber);
+        var getResponse = await _client.ExecuteGetAsync(getRequest);
         getResponse.IsSuccessful.Should().BeTrue();
-        getResponse.Data!.Number.Should().Be(invoiceNumber);
+        var retrievedInvoice = Deserialize(getResponse);
+        retrievedInvoice.Number.Should().Be(invoiceNumber);
     }
 
     [Fact]
@@ -74,9 +84,9 @@ public class InvoiceApiTests : IClassFixture<TestWebApplicationFactory>
         };
 
         var soPost = new RestRequest("/SalesOrder").AddJsonBody(soRequest);
-        var soResponse = await _client.ExecutePostAsync<InvoiceResponse>(soPost);
+        var soResponse = await _client.ExecutePostAsync(soPost);
         soResponse.IsSuccessful.Should().BeTrue();
-        var invoiceNumber = soResponse.Data!.Number;
+        var invoiceNumber = Deserialize(soResponse).Number!;
 
         var wrRequest = new WarehouseReleaseRequested
         {
@@ -100,13 +110,19 @@ public class InvoiceApiTests : IClassFixture<TestWebApplicationFactory>
         };
 
         var wrPost = new RestRequest("/WarehouseMovement/warehouseRelease").AddJsonBody(wrRequest);
-        var wrResponse = await _client.ExecutePostAsync<InvoiceResponse>(wrPost);
+        var wrResponse = await _client.ExecutePostAsync(wrPost);
         wrResponse.IsSuccessful.Should().BeTrue();
 
-        var getRequest = new RestRequest($"/Invoice/{invoiceNumber}");
-        var getResponse = await _client.ExecuteGetAsync<InvoiceResponse>(getRequest);
+        var getRequest = new RestRequest("/Invoice/{number}").AddUrlSegment("number", invoiceNumber);
+        var getResponse = await _client.ExecuteGetAsync(getRequest);
         getResponse.IsSuccessful.Should().BeTrue();
-        getResponse.Data!.GrossValue.Should().BeGreaterThan(0m);
+        Deserialize(getResponse).GrossValue.Should().BeGreaterThan(0m);
+    }
+
+    private static InvoiceResponse Deserialize(RestResponse response)
+    {
+        response.Content.Should().NotBeNullOrWhiteSpace();
+        return JsonSerializer.Deserialize<InvoiceResponse>(response.Content!, SerializerOptions)!;
     }
 }
 
